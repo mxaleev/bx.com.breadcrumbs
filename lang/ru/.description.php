a1dd2f74924c9560e243a2cbb986f2a4ded681c9
Mikhail Khaleev <mxaleev@gmail.com>
2021-04-08 22:51:35 +0300

    Init

---
+++ lang/ru/.description.php
@@ -0,0 +1,10 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$MESS["COMPONENT_PATH_ID"] = "xaleev";
+$MESS["COMPONENT_PATH_NAME"] = "XALEEV";
+
+$MESS["COMPONENT_CHILD_PATH_ID"] = "breadcrumbs";
+$MESS["COMPONENT_CHILD_PATH_NAME"] = "Хлебные крошки";
+
+$MESS ['COMPONENT_NAME'] = "Хлебные крошки";
+$MESS ['COMPONENT_DESCRIPTION'] = "Хлебные крошки";
---
+++ templates/.default/template.php
@@ -0,0 +1,24 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
+$this->setFrameMode(false);
+
+//use Bitrix\Main\Page\Asset;
+//Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
+
+//dump_r($arResult);
+?>
+
+<div class="breadcrumbs">
+[breadcrumbs]
+</div>
